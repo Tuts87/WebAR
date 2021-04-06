6ce323abe3e8d0dd2cc78480b5cdbe2ce91944dc
Glitch (hello-webpage) <none>
2021-04-06 15:44:36 +0000

    🐦👜 Checkpoint ./script.js:27255193/658 ./index.html:27255193/6 ./.glitch-assets:27255193/859

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-const modelViewerColor = document.querySelector("model-viewer#color");
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
 
-document.querySelector('#color-controls').addEventListener('click', (event) => {
-  const colorString = event.target.dataset.color;
-
-  if (!colorString) {
-    return;
-  }
-
-  const color = colorString.split(',')
-      .map(numberString => parseFloat(numberString));
-
-  console.log('Changing color to: ', color);
-  const [material] = modelViewerColor.model.materials;
-  material.pbrMetallicRoughness.setBaseColorFactor(color);
-});
+// prints "hi" in the browser's dev tools console
+console.log("hi");
